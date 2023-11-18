4c1742bdcdcd509aa1ccd3ce98a259aaab97b4f8
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-11-18 14:32:51 +0000

    get echo cmd working

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,29 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::process::Command;
+
+#[tauri::command]
+fn echo_cmd(text: &str) -> String {
+  let output = if cfg!(target_os = "windows") {
+    Command::new("cmd")
+      .args(&["/C", "echo", text])
+      .output()
+      .expect("failed to execute process")
+  } else {
+    Command::new("sh")
+      .arg("-c")
+      .arg(format!("echo {}", text))
+      .output()
+      .expect("failed to execute process")
+  };
+
+  format!("echo: {}", String::from_utf8_lossy(&output.stdout))
+}
+
 fn main() {
     tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![echo_cmd])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
+  import { invoke } from '@tauri-apps/api/tauri'
+
+  let echo_text = ''
+  let response = ''
+
+  async function echo() {
+    response = await invoke('echo_cmd', { text: echo_text })
+  }
 </script>
 
 <main class="container">
+  <input type="text" bind:value={echo_text} />
+  <button on:click={echo}>Echo</button>
+
+  <p>{response}</p>
 </main>
 
 <style>
