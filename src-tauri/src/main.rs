b4e53e8e1dc0cb4971abd63995a726904eaca41c
Bryan McCarthy <bryanmccarthy2@gmail.com>
2023-11-18 15:53:48 +0000

    convert cmd

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -14,9 +14,21 @@ fn echo_cmd(text: &str) -> String {
   format!("echo: {}", String::from_utf8_lossy(&output.stdout))
 }
 
+#[tauri::command]
+fn convert_cmd(file: &str, output: &str) -> String {
+  println!("command: convert {} {}", file, output);
+
+  let output = Command::new("sh")
+                        .arg(format!("convert {} {}", file, output))
+                        .output()
+                        .expect("failed to execute process");
+
+  format!("convert: {}", String::from_utf8_lossy(&output.stdout))
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![echo_cmd])
+        .invoke_handler(tauri::generate_handler![echo_cmd, convert_cmd])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,17 @@
   import { invoke } from '@tauri-apps/api/tauri'
 
   let echo_text = ''
-  let response = ''
+  let echo_response = ''
+  let file_path = ''
+  let output_path = ''
+  let convert_response = ''
 
   async function echo() {
-    response = await invoke('echo_cmd', { text: echo_text })
+    echo_response = await invoke('echo_cmd', { text: echo_text })
+  }
+
+  async function convert() {
+    convert_response = await invoke('convert_cmd', { file: file_path, output: output_path })
   }
 </script>
 
@@ -13,7 +20,13 @@
   <input type="text" bind:value={echo_text} />
   <button on:click={echo}>Echo</button>
 
-  <p>{response}</p>
+  <p>{echo_response}</p>
+
+  <input type="text" bind:value={file_path} />
+  <input type="text" bind:value={output_path} />
+  <button on:click={convert}>Convert</button>
+
+  <p>{convert_response}</p>
 </main>
 
 <style>
